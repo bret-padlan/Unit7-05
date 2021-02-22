3f79ef823f2d7f5ccd186bfad6507bf401e0cb0b
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-22 18:34:13 +0000

    JS, Line 9, added "=" to "!="

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let age = 18
 function dayAge () {
   age = document.getElementById('table').value
   day = document.getElementById('table-two').value
-  if (age > 18 && day != 'Saturday' && day != 'Sunday') {
+  if (age > 18 && day !== 'Saturday' && day !== 'Sunday') {
     document.getElementById('name').innerHTML = ('Time to work')
   } else if ((day === 'Saturday') || (day === 'Sunday')) {
     document.getElementById('name').innerHTML = ('Weekend!')
